6cdf92d6c2bb94e7e81954fb35924a5656e7e7c1
abhaysp95 <abhaysp95@protonmail.com>
2024-02-21 23:10:02 +0530

    feat: cleaning up unnecessary controller and restructuring project

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,12 @@
-use actix_web::{HttpResponse, HttpServer, Responder};
+use actix_web::{HttpServer, Responder};
 use std::net::TcpListener;
 
-async fn health() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-async fn greet(name: String) -> impl Responder {
-    format!(
-        "Hello, {}",
-        if name.is_empty() {
-            String::from("World")
-        } else {
-            name
-        }
-    )
-}
+mod controller;
 
 pub fn run(listener: TcpListener) -> Result<actix_web::dev::Server, std::io::Error> {
     let server = HttpServer::new(|| {
         actix_web::App::new()
-            .route("/health", actix_web::web::get().to(health))
-            .route("/greet/{name}", actix_web::web::get().to(greet))
+            .route("/health", actix_web::web::get().to(controller::health))
     })
     .listen(listener)?
     .run();
